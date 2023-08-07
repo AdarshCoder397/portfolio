affa612239facebedc589d1ddba46668f24d2756
Anubha13kumari <anubhamuz@gmail.com>
2023-08-07 13:45:25 +0530

    Second Push

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -14,20 +14,28 @@ const Services = () => {
             <div className="services__content-info">
               <h2 className="w1">Website Development</h2>
               <span className="section__subtitle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                nobis non pariatur magnam dicta molestiae recusandae beatae
-                soluta quisquam rem! Quisquam repellendus, velit dolorem
-                doloremque sapiente commodi nisi, fugiat veniam a ad corrupti
-                totam odio reiciendis! Consectetur consequatur soluta tempore
-                excepturi alias vel omnis at amet, libero nostrum atque quam
-                architecto. Ad distinctio ratione ipsam molestias praesentium,
-                odio fugit illo ea fuga quibusdam pariatur? Fugit repellat
-                incidunt doloremque.
+                Hello there! If you're looking for top-notch web development
+                services, you've come to the right place. I offer a wide range
+                of website building options to cater to all your needs. Whether
+                you need an online store, a professional business site, a
+                personal portfolio, or anything else, I've got it covered. My
+                goal is to make your website not only look fantastic but also
+                help your business thrive. With user-friendly design and the
+                latest tech, I'll help you attract more customers, increase
+                conversions, and boost your overall success. Let's build
+                something amazing together!
               </span>
+              <a href="#contact" className="button button-flex">
+                Get a Quote
+              </a>
             </div>
-            <div className="services__content-img"></div>
+            <div className="services__content-img">
+              <img
+                src="https://www.webodesk.com/themes/demo/assets/img/responsive-animation.gif"
+                alt=""
+              />
+            </div>
           </div>
-          {/* buto */}
         </div>
       </div>
     </section>
